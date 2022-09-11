5a9b13163b032141ea78b4320201718316794514
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2022-09-11 18:35:49 -0400

    fix: max zoom

--- src/components/Mapa.js
+++ src/components/Mapa.js
@@ -8,7 +8,9 @@ import MarkerClusterGroup from 'react-leaflet-cluster'
 export default function Mapa() {
 
     return (
-        <MapContainer center={[-17.394, -66.161]} zoom={13.5} attributionControl={false} className="map-container">
+        <MapContainer center={[-17.394, -66.161]} zoom={13.5} attributionControl={false} className="map-container"
+        maxZoom={15}
+        >
             <TileLayer
                 attribution='<a href="https://labtecnosocial.org/">Lab TecnoSocial</a>'
                 url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png"
