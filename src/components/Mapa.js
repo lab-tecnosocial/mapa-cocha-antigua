b2067e60f1722f2e731c77077949c2f0f3f06a50
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2022-09-11 09:08:27 -0400

    con datos

--- src/components/Mapa.js
+++ src/components/Mapa.js
@@ -1,16 +1,23 @@
-import { MapContainer, TileLayer, useMap, Popup, Marker } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Popup, Marker, Circle } from 'react-leaflet'
+import data from '../data/cocha-antigua.json'
+
 
 export default function Mapa() {
     return (
-        <MapContainer center={[-17.394, -66.161]} zoom={13.5} scrollWheelZoom={false} className="map-container">
+        <MapContainer center={[-17.394, -66.161]} zoom={13.5} attributionControl={false} className="map-container">
             <TileLayer
+                attribution = '<a href="https://labtecnosocial.org/">Lab TecnoSocial</a>'
                 url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png"
             />
-            <Marker position={[-17.394, -66.161]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+            {data.map((item, index) => (
+                <Marker key={index} position={item.coordArray}>
+                    <Popup>
+                        <img src={item.imgUrl} width="300px"/>
+                        <h3></h3>
+                        <p>{item.anio}</p>
+                    </Popup>
+                </Marker>
+            ))}
         </MapContainer>
     )
 }
