9de3216f3f0015ce4c002dc2e24c6b37c4f65b4d
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2024-09-25 19:02:26 -0400

    update basemap

--- src/components/Mapa.js
+++ src/components/Mapa.js
@@ -8,22 +8,21 @@ export default function Mapa() {
 
     return (
         <MapContainer center={[-17.394, -66.161]} zoom={13.5} attributionControl={false} className="map-container"
-        maxZoom={15}
         >
             <TileLayer
                 attribution='Desarrollado por el <a href="https://labtecnosocial.org/">Lab TecnoSocial</a>'
-                url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png"
+                url="https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png"
             />
-             <MarkerClusterGroup
-        chunkedLoading
-        spiderLegPolylineOptions={{color: '#C9821E'}}
-        polygonOptions={{color: '#C9821E'}}
-        maxClusterRadius={40}
+            <MarkerClusterGroup
+                chunkedLoading
+                spiderLegPolylineOptions={{ color: '#C9821E' }}
+                polygonOptions={{ color: '#C9821E' }}
+                maxClusterRadius={40}
 
-      >
-            {data.map((item, index) => (
-                <MiMarker key={index} item={item} />
-            ))}
+            >
+                {data.map((item, index) => (
+                    <MiMarker key={index} item={item} />
+                ))}
             </MarkerClusterGroup>
             <AttributionControl position="bottomleft" prefix={false} className="atribucion" />
 
